f301875764e93e972fc27e2516085b19ec124f27
Giraphi <hoeps.raphael@gmail.com>
2023-07-05 15:58:16 +0200

    ascii render

---
+++ app/components/MeModel/MeContent.tsx
@@ -0,0 +1,99 @@
+import React, { useRef } from "react";
+import { AsciiRenderer, Box, OrbitControls, Sphere, useGLTF } from "@react-three/drei";
+import { Group, Vector3 } from "three";
+import { useFrame } from "@react-three/fiber";
+import { GLTF } from "three-stdlib";
+
+type GLTFResult = GLTF & {
+    nodes: {
+        mesh_0: THREE.Mesh;
+    };
+    materials: {};
+};
+
+export interface MeContentProps {}
+
+export default function MeContent(props: MeContentProps) {
+    const { nodes } = useGLTF("/models/me.glb") as GLTFResult;
+    const rotateRef = useRef<Group>(null);
+    const rotate2Ref = useRef<Group>(null);
+
+    useFrame((state, delta) => {
+        // if (!rotateRef.current) {
+        //     return;
+        // }
+        rotateRef.current?.rotateY(delta * 2);
+        rotate2Ref.current?.rotateY(delta * 1.2);
+    });
+
+    return (
+        <>
+            <color
+                attach="background"
+                args={["black"]}
+            />
+
+            <AsciiRenderer
+                invert={true}
+                bgColor={"transparent"}
+                resolution={0.18}
+                characters={" .:-+*=%@#FXQRI,?'`´"}
+            />
+
+            <OrbitControls />
+            {/*<pointLight*/}
+            {/*    intensity={0.5}*/}
+            {/*    color={"white"}*/}
+            {/*    position={[0, 20, 10]}*/}
+            {/*    castShadow={true}*/}
+            {/*/>*/}
+
+            <group
+                position={[0, 0, 0]}
+                ref={rotateRef}
+            >
+                <Sphere
+                    scale={0.1}
+                    position={[1.4, 1, 0.7]}
+                />
+            </group>
+
+            <group
+                position={[0, 0, 0]}
+                ref={rotate2Ref}
+            >
+                <Sphere
+                    scale={0.1}
+                    position={[1.8, 0.5, -0.2]}
+                />
+            </group>
+
+            <spotLight
+                intensity={1}
+                color={"white"}
+                position={[5, 15, 10]}
+                castShadow={true}
+            />
+
+            {/*<ambientLight intensity={0.1} />*/}
+            {/*<pointLight*/}
+            {/*    distance={1000}*/}
+            {/*    decay={0.1}*/}
+            {/*    castShadow={true}*/}
+            {/*    intensity={0.6}*/}
+            {/*    color={"white"}*/}
+            {/*    position={[3, 2, 3]}*/}
+            {/*/>*/}
+
+            {/*<gridHelper />*/}
+
+            <mesh
+                geometry={nodes.mesh_0.geometry}
+                material={nodes.mesh_0.material}
+                rotation={[0.05, -0.1, 0.07]}
+                position={[0, -0.5, 0]}
+                scale={12}
+            />
+        </>
+    );
+}
--- app/components/MeModel/MeModel.tsx
+++ app/components/MeModel/MeModel.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { AsciiRenderer, OrbitControls, useGLTF } from "@react-three/drei";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { AsciiRenderer, OrbitControls, Sphere, useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
+import { Group, Vector3 } from "three";
+import MeContent from "@/app/components/MeModel/MeContent";
 
 type GLTFResult = GLTF & {
     nodes: {
@@ -15,52 +17,9 @@ type GLTFResult = GLTF & {
 export interface MeModelProps {}
 
 export default function MeModel(props: MeModelProps) {
-    const { nodes } = useGLTF("/models/me.glb") as GLTFResult;
-
     return (
         <Canvas style={{ height: "100%" }}>
-            <color
-                attach="background"
-                args={["black"]}
-            />
-
-            <AsciiRenderer
-                invert={true}
-                bgColor={"transparent"}
-                resolution={0.2}
-            />
-
-            <OrbitControls />
-            {/*<pointLight*/}
-            {/*    intensity={0.5}*/}
-            {/*    color={"white"}*/}
-            {/*    position={[0, 20, 10]}*/}
-            {/*    castShadow={true}*/}
-            {/*/>*/}
-
-            <spotLight
-                intensity={1}
-                color={"white"}
-                position={[5, 25, 10]}
-                castShadow={true}
-            />
-
-            {/*<ambientLight intensity={0.1} />*/}
-            {/*<pointLight*/}
-            {/*    distance={1000}*/}
-            {/*    decay={0.1}*/}
-            {/*    castShadow={true}*/}
-            {/*    intensity={0.6}*/}
-            {/*    color={"white"}*/}
-            {/*    position={[3, 2, 3]}*/}
-            {/*/>*/}
-
-            <mesh
-                geometry={nodes.mesh_0.geometry}
-                material={nodes.mesh_0.material}
-                rotation={[-0.1, 0.08, 0.07]}
-                scale={8.62}
-            />
+            <MeContent />
         </Canvas>
     );
 }
