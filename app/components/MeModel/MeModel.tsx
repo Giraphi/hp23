346ecb63b0d658823efcca9af32f47a5545d91a0
Giraphi <hoeps.raphael@gmail.com>
2023-07-13 19:02:34 +0200

    remove rotation hint

--- app/components/MeModel/MeModel.tsx
+++ app/components/MeModel/MeModel.tsx
@@ -11,7 +11,7 @@ export interface MeModelProps {}
 export default function MeModel(props: MeModelProps) {
     const ref = useRef<HTMLDivElement>(null);
     const { scrollYProgress } = useScroll({ target: ref, offset: ["start 35%", "end start"] });
-    const hintOpacity = useTransform(scrollYProgress, [0.3, 0.4], [0, 1]);
+    // const hintOpacity = useTransform(scrollYProgress, [0.3, 0.4], [0, 1]);
 
     return (
         <>
@@ -24,12 +24,12 @@ export default function MeModel(props: MeModelProps) {
                     <Canvas className={styles.canvasWrapper}>
                         <MeContent scrollProgress={scrollYProgress} />
                     </Canvas>
-                    <motion.div
-                        className={"absolute top-0 w-full bg-gray-2 p-1 text-center text-sm text-lime"}
-                        style={{ opacity: hintOpacity }}
-                    >
-                        Swipe right/left to rotate
-                    </motion.div>
+                    {/*<motion.div*/}
+                    {/*    className={"absolute top-0 w-full bg-gray-2 p-1 text-center text-sm text-lime"}*/}
+                    {/*    style={{ opacity: hintOpacity }}*/}
+                    {/*>*/}
+                    {/*    Swipe right/left to rotate*/}
+                    {/*</motion.div>*/}
                 </div>
             </div>
         </>
