4f38a2afdb34fd34d0d1f6e475b439dd6d706385
Giraphi <hoeps.raphael@gmail.com>
2023-07-13 18:23:50 +0200

    suspense

--- app/components/MeModel/MeContent.tsx
+++ app/components/MeModel/MeContent.tsx
@@ -1,31 +1,20 @@
-import React, { useRef, useState } from "react";
-import { AsciiRenderer, OrbitControls, Sphere, useGLTF } from "@react-three/drei";
-import { Group, MOUSE, TOUCH } from "three";
+import React, { Suspense, useRef } from "react";
+import { AsciiRenderer, Sphere } from "@react-three/drei";
+import { Group } from "three";
 import { useFrame, useThree } from "@react-three/fiber";
-import { GLTF } from "three-stdlib";
-import { Device, useDeviceStore } from "@/app/store/useDeviceStore";
-import * as THREE from "three";
-import { OrbitControls as OrbitControlsType } from "three-stdlib";
 import ControlsSmall from "@/app/components/MeModel/ControlsSmall";
 import { MotionValue } from "framer-motion";
-
-type GLTFResult = GLTF & {
-    nodes: {
-        mesh_0: THREE.Mesh;
-    };
-    materials: {};
-};
+import MeGltf from "@/app/components/MeModel/MeGltf";
 
 export interface MeContentProps {
     scrollProgress: MotionValue<number>;
 }
 
 export default function MeContent(props: MeContentProps) {
-    const { nodes } = useGLTF("/models/me.glb") as GLTFResult;
+    // const result = useGLTF("/models/me.glb") as GLTFResult;
     const rotateRef = useRef<Group>(null);
     const rotate2Ref = useRef<Group>(null);
     const { viewport } = useThree();
-    const device = useDeviceStore((state) => state.device);
 
     useFrame((state, delta) => {
         rotateRef.current?.rotateY(delta * 2);
@@ -78,13 +67,16 @@ export default function MeContent(props: MeContentProps) {
                         castShadow={true}
                     />
 
-                    <mesh
-                        geometry={nodes.mesh_0.geometry}
-                        material={nodes.mesh_0.material}
-                        rotation={device < Device.md ? [0.05, -0.1, 0.07] : [0.05, -0.4, 0.07]}
-                        position={device < Device.md ? [-0.2, -0.6, 0] : [0, -0.6, 0]}
-                        scale={12}
-                    />
+                    <Suspense>
+                        <MeGltf />
+                    </Suspense>
+                    {/*<mesh*/}
+                    {/*    geometry={nodes.mesh_0.geometry}*/}
+                    {/*    material={nodes.mesh_0.material}*/}
+                    {/*    rotation={device < Device.md ? [0.05, -0.1, 0.07] : [0.05, -0.4, 0.07]}*/}
+                    {/*    position={device < Device.md ? [-0.2, -0.6, 0] : [0, -0.6, 0]}*/}
+                    {/*    scale={12}*/}
+                    {/*/>*/}
                 </group>
             </group>
         </>
---
+++ app/components/MeModel/MeGltf.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+import { GLTF } from "three-stdlib";
+import * as THREE from "three";
+import { Device, useDeviceStore } from "@/app/store/useDeviceStore";
+
+type GLTFResult = GLTF & {
+    nodes: {
+        mesh_0: THREE.Mesh;
+    };
+    materials: {};
+};
+
+export interface MeGltfProps {}
+
+useGLTF.preload("/models/me.glb");
+
+export default function MeGltf(props: MeGltfProps) {
+    const { nodes } = useGLTF("/models/me.glb") as GLTFResult;
+    const device = useDeviceStore((state) => state.device);
+
+    return (
+        <>
+            <mesh
+                geometry={nodes.mesh_0.geometry}
+                material={nodes.mesh_0.material}
+                rotation={device < Device.md ? [0.05, -0.1, 0.07] : [0.05, -0.4, 0.07]}
+                position={device < Device.md ? [-0.2, -0.6, 0] : [0, -0.6, 0]}
+                scale={12}
+            />
+        </>
+    );
+}
