9e89bc3ce839a552fecd34c68d0fdc54ee9cee6a
Giraphi <hoeps.raphael@gmail.com>
2023-07-13 18:58:47 +0200

    install sass to fix server build

--- app/components/MeModel/MeContent.tsx
+++ app/components/MeModel/MeContent.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useRef, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import { AsciiRenderer, Sphere } from "@react-three/drei";
 import { Group } from "three";
 import { useFrame, useThree } from "@react-three/fiber";
