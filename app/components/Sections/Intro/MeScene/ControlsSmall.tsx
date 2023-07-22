31a7db20034e874aacbb2748186dd8f360c4c969
Giraphi <hoeps.raphael@gmail.com>
2023-07-22 10:04:13 +0200

    experimental: zoom with low frequency but smooth between steps

--- app/components/Sections/Intro/MeScene/ControlsSmall.tsx
+++ app/components/Sections/Intro/MeScene/ControlsSmall.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { CameraControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { MotionValue, useTransform } from "framer-motion";
+import { MotionValue, useMotionValueEvent, useTransform } from "framer-motion";
 
 export interface ControlsSmallProps {
     scrollProgress: MotionValue<number>;
@@ -13,8 +13,19 @@ export default function ControlsSmall(props: ControlsSmallProps) {
     const zoom = useTransform(props.scrollProgress, [0, 0.1, 1], [0.18, 0.35, 3]);
 
     useFrame(() => {
-        controlsRef.current?.zoomTo(zoom.get());
+        const target = zoom.get();
+        const current = controlsRef.current._zoom;
+
+        if (Math.abs(target - current) < 0.05) {
+            return;
+        }
+
+        controlsRef.current?.zoomTo(target, true);
     });
+
+    // useMotionValueEvent(zoom, "change", (value) => {
+    //     controlsRef.current?.zoomTo(value);
+    // });
 
     const { onReady } = props;
     useEffect(() => {
@@ -25,6 +36,7 @@ export default function ControlsSmall(props: ControlsSmallProps) {
         <>
             <CameraControls
                 touches={{ one: 32, two: 0, three: 0 }}
+                smoothTime={0.15}
                 mouseButtons={{ wheel: 0, left: 1, right: 1, middle: 0 }}
                 ref={controlsRef}
                 minPolarAngle={Math.PI / 2}
