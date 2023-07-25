cefb672702effbaef2ac78d29164958ada56dc71
Giraphi <hoeps.raphael@gmail.com>
2023-07-25 21:35:44 +0200

    cleanup

--- app/components/Sections/Intro/MeScene/MeScene.tsx
+++ app/components/Sections/Intro/MeScene/MeScene.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import MeCanvas from "@/app/components/Sections/Intro/MeScene/MeCanvas";
-import { MotionValue, motion, useTransform, useMotionValueEvent, MotionConfig } from "framer-motion";
+import { MotionValue, motion, useTransform } from "framer-motion";
 import { Device, useDeviceStore } from "@/app/store/useDeviceStore";
 import { MotionCanvas } from "framer-motion-3d";
-import { Canvas } from "@react-three/fiber";
-import { current } from "immer";
 
 export interface MeSceneProps {
     scrollYProgress: MotionValue<number>;
@@ -19,23 +17,6 @@ export default function MeScene(props: MeSceneProps) {
     const heightPercentage = Math.floor((props.textContentHeight * 100) / windowHeight);
     const spaceTop = device <= Device.sm ? heightPercentage - 32 : heightPercentage - 25;
     const y = useTransform(props.scrollYProgress, [0, 1], [`0lvh`, "90lvh"]);
-    // const [size, setSize] = useState("");
-    // const timeout = useRef<NodeJS.Timeout>();
-
-    // Workaround to make MotionCanvas adjust to new window sizes.
-    // The MotionCanvas component seems to set the dimensions only once at mount:
-    // https://github.com/framer/motion/blob/34b97c7be35a6774f675fb8f1e17f161fa848cc9/packages/framer-motion-3d/src/components/MotionCanvas.tsx
-    // useEffect(() => {
-    //     function handleResize() {
-    //         if (!!timeout.current) clearTimeout(timeout.current);
-    //         timeout.current = setTimeout(() => {
-    //             setSize(`${window.innerHeight}${window.innerWidth}`);
-    //         }, 100);
-    //     }
-    //
-    //     window.addEventListener("resize", handleResize);
-    //     return () => window.removeEventListener("resize", handleResize);
-    // }, []);
 
     return (
         <>
