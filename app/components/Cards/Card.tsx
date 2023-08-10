6381f9a4f8412c7628d2005bc49007fb41c5525a
Giraphi <hoeps.raphael@gmail.com>
2023-08-11 01:30:09 +0200

    card animation

--- app/components/Cards/Card.tsx
+++ app/components/Cards/Card.tsx
@@ -10,14 +10,18 @@ export interface CardProps {
 
 export default function Card(props: CardProps) {
     return (
-        <CardAppear className={"flex w-full max-w-md flex-col gap-4 rounded-2xl bg-gray-1 p-4 md:p-8 lg:text-base xl:gap-8 xl:text-xl"}>
+        <CardAppear
+            className={
+                "group flex w-full max-w-md cursor-pointer flex-col gap-4 rounded-2xl bg-gray-1 p-4 md:p-8 lg:text-base xl:gap-8 xl:text-xl"
+            }
+        >
             <div className={"relative z-20 aspect-image overflow-hidden rounded-xl"}>
                 <Image
                     quality={90}
                     src={props.image}
                     alt={"project picture"}
                     fill={true}
-                    className={"object-cover"}
+                    className={"object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"}
                     sizes={"400px"}
                 />
             </div>
