8c24acbe1389568bf672008e113a1ae4e589ce0c
Giraphi <hoeps.raphael@gmail.com>
2023-08-10 18:51:05 +0200

    demo component

---
+++ app/code-examples/scroll/ScrollAnimationContainer.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import ScrollAnimation from "@/app/code-examples/scroll/ScrollAnimation/ScrollAnimation";
+
+export interface AnimationWrapperProps {}
+
+export default function ScrollAnimationContainer(props: AnimationWrapperProps) {
+    return <ScrollAnimation />;
+}
