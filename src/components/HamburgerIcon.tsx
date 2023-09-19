d14466d3aa91ec9595c2b23ce6c920510d6d09cb
Giraphi <hoeps.raphael@gmail.com>
2023-09-20 01:01:07 +0200

    animate hamburger

--- src/components/HamburgerIcon.tsx
+++ src/components/HamburgerIcon.tsx
@@ -38,15 +38,17 @@ export default function HamburgerIcon(props: HamburgerIconProps) {
                     d={"M10 18h28s2 0 2 2v0s0 2 -2 2h-28s-2 0 -2 -2v0s0 -2 2 -2"}
                     initial={props.isCross ? firstRectVariants.cross : firstRectVariants.hamburger}
                     animate={props.isCross ? firstRectVariants.cross : firstRectVariants.hamburger}
-                    style={{ originX: "50%", originY: "50%" }}
+                    style={{ originX: "42%", originY: "50%" }}
                     fill="currentColor"
+                    transition={{ ease: "linear", duration: 0.15 }}
                 />
                 <motion.path
                     d={"M10 26h28s2 0 2 2v0s0 2 -2 2h-28s-2 0 -2 -2v0s0 -2 2 -2"}
                     initial={props.isCross ? secondRectVariants.cross : secondRectVariants.hamburger}
                     animate={props.isCross ? secondRectVariants.cross : secondRectVariants.hamburger}
-                    style={{ originX: "50%", originY: "50%" }}
+                    style={{ originX: "42%", originY: "50%" }}
                     fill="currentColor"
+                    transition={{ ease: "linear", duration: 0.15 }}
                 />
             </svg>
         </>
